4276e04b6e1d797e2317b7db896e33167ca72813
fluktar <tyc.pawel@gmail.com>
2024-01-30 15:59:47 +0000

    add link

--- index.php
+++ index.php
@@ -9,10 +9,11 @@ namespace App;
 
 require('src/utils/debug.php');
 
-
-$test = "  ==== PHP 1 PROJECT ====";
-
-
+if (!empty($_GET['action'])) {
+    $action = $_GET['action'];
+} else {
+    $action = null;
+}
 
 ?>
 
@@ -30,8 +31,20 @@ $test = "  ==== PHP 1 PROJECT ====";
         <h1>Moje notatki</h1>
     </div>
     <div class="container">
-        <div class="menu"></div>
-        <div>Treść</div>
+        <div class="menu">
+            <ul>
+                <li><a href="/">Lista notatek</a></li>
+                <li><a href="/?action=create">Nowa notatka</a></li>
+            </ul>
+        </div>
+        <div><?php
+                if ($action === 'create') :
+                    echo "nowa notatka";
+                else :
+                    echo "lista notatek";
+                endif;
+                ?></div>
+
     </div>
     <div class="footer">
 
