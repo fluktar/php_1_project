9b402aab6ae761ec841c454909174502285bdb7b
fluktar <tyc.pawel@gmail.com>
2024-01-30 15:38:24 +0000

    Notes - PHP project - init

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+// include('src/utils/debug.php');
+// include_once('src/utils/debug.php');
+
+require('src/utils/debug.php');
+
+
+$test = "  ==== PHP 1 PROJECT ====";
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="header">
+        <h1>Moje notatki</h1>
+    </div>
+    <div class="container">
+        <div class="menu"></div>
+        <div>Treść</div>
+    </div>
+    <div class="footer">
+
+    </div>
+</body>
+
+</html>
