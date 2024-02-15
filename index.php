5dd48b2d288fceda315c0bb4d5317deac7d1ad73
Paweł <tyc.pawel@gmail.com>
2024-02-15 14:21:25 +0100

    prepare aplication for new feature and controller like user, note, category ...

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ use App\Exception\ConfigurationException;
 use Throwable;
 
 require_once("src/Utils/debug.php");
-require_once("src/Controller.php");
+require_once("src/NoteController.php");
 require_once("src/Exception/AppException.php");
 require_once('src/Request.php');
 
@@ -22,8 +22,8 @@ try {
   //$controller = new Controller($request);
   //$controller->run();
 
-  Controller::initConfiguration($configuration);
-  (new Controller($request))->run();
+  AbstractController::initConfiguration($configuration);
+  (new NoteController($request))->run();
 } catch (ConfigurationException $e) {
   //mail('xxx@xxx.com', 'Errro', $e->getMessage());
   echo '<h1>Wystąpił błąd w aplikacji</h1>';
---
+++ src/NoteController.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+require_once("AbstractController.php");
+
+use App\Exception\NotFoundException;
+
+class  NoteController extends AbstractController
+{
+  public function createAction()
+  {
+    if ($this->request->hasPost()) {
+      $noteData = [
+        'title' => $this->request->postParam('title'),
+        'description' => $this->request->postParam('description'),
+      ];
+      $this->database->createNote($noteData);
+      header('Location: /phptesty/?before=created');
+      exit;
+    }
+    $this->view->render(
+      'create',
+    );
+  }
+  public function showAction()
+  {
+    $noteId = (int) $this->request->getParam('id');
+
+    if (!$noteId) {
+      header('Location: /phptesty/?error=missingNoteId');
+      exit;
+    }
+    try {
+      $note = $this->database->getNote($noteId);
+    } catch (NotFoundException $th) {
+      header('Location: /phptesty/?error=noteNotFound');
+      exit;
+    }
+
+    $this->database->getNote($noteId);
+
+    $viewParams = [
+      'note' => $note
+    ];
+    $this->view->render('show', ['note' => $note]);
+  }
+  public function listAction()
+  {
+    $page = 'list';
+
+    $this->view->render('list', [
+      'notes' => $this->database->getNotes(),
+      'before' => $this->request->getParam('before'),
+      'error' => $this->request->getParam('error'),
+    ]);
+  }
+}
